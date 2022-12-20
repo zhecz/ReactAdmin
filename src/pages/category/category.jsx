118976795da2452695049de9e4a0fd24f765118f
zhe <youcanfindivy@gmail.com>
2022-12-19 22:28:02 -0500

    Category component: dynamically display the first level category list

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,9 +1,98 @@
 import React, { Component } from 'react'
-import { Card, Button, Table, Modal } from 'antd'
+import { Card, Button, Table, Modal, message } from 'antd'
 import LinkButton from '../../components/link-button';
+import {reqCategorys, reqUpdateCategory, reqAddCategory} from '../../api'
 
 export default class Category extends Component {
+
+  state = {
+    loading: false, // 是否正在获取数据中
+    categorys: [], // 一级分类列表
+    /* subCategorys: [], // 二级分类列表
+    parentId: '0', // 当前需要显示的分类列表的父分类ID
+    parentName: '', // 当前需要显示的分类列表的父分类名称
+    showStatus: 0, // 标识添加/更新的确认框是否显示, 0: 都不显示, 1: 显示添加, 2: 显示更新 */
+  }
+
+ /*
+  初始化Table所有列的数组
+   */
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '分类的名称',
+        dataIndex: 'name', // 显示数据对应的属性名
+      },
+      {
+        title: '操作',
+        width: 300,
+        render: () => ( // 返回需要显示的界面标签
+          <span>
+            <LinkButton>修改分类</LinkButton>
+           
+            <LinkButton>查看子分类</LinkButton>
+
+          </span>
+        )
+      }
+    ]
+  }
+
+  /*
+  异步获取一级/二级分类列表显示
+  parentId: 如果没有指定根据状态中的parentId请求, 如果指定了根据指定的请求
+   */
+  getCategorys = async () => {
+
+    // 在发请求前, 显示loading
+    this.setState({loading: true})
+    
+    // 发异步ajax请求, 获取数据
+    const result = await reqCategorys('0')
+    // 在请求完成后, 隐藏loading
+    this.setState({loading: false})
+
+    if(result.status===0) {
+      // 取出分类数组(可能是一级也可能二级的)
+      const categorys = result.data
+     
+        // 更新一级分类状态
+        this.setState({
+          categorys
+        })
+        console.log('----', this.state.categorys.length)
+     
+      
+    } else {
+      message.error('获取分类列表失败')
+    }
+  }
+
+
+
+  /*
+  为第一次render()准备数据
+   */
+  componentWillMount () {
+    this.initColumns()
+  }
+
+  /*
+  执行异步任务: 发异步ajax请求
+   */
+  componentDidMount () {
+    // 获取一级分类列表显示
+    this.getCategorys()
+  }
+
+
+
+
+
   render() {
+
+    // 读取状态数据
+    const {categorys, loading} = this.state
 
     const title = '一级分类列表'
     const extra = (
@@ -61,8 +150,11 @@ export default class Category extends Component {
     return (
       <Card title={title} extra={extra}>
       <Table
-      dataSource={dataSource}
-      columns={columns}
+       loading={loading}
+       dataSource={categorys}
+       columns={this.columns}
+       pagination={{defaultPageSize: 5, showQuickJumper: true}}
+     
       rowKey='_id' // 设置key值,不然会报错
       bordered
       />
