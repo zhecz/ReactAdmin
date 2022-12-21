6c994486a891a6226565efa67bac9b994e126a2f
zhe <youcanfindivy@gmail.com>
2022-12-21 12:43:56 -0500

    Product component: adding first level category list for productAddUpdate component

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -19,6 +19,176 @@ const { TextArea } = Input
 
 export default class ProductAddUpdate extends Component {
 
+  state = {
+    options: [],
+  }
+
+  initOptions = (categorys) => {
+    // 根据categorys生成options数组
+    const options = categorys.map(c => ({
+      value: c._id,
+      label: c.name,
+      isLeaf: false, // 不是叶子
+    }))
+
+    this.setState({
+      options
+    })
+
+   /*  // 如果是一个二级分类商品的更新
+    const {isUpdate, product} = this
+    const {pCategoryId} = product
+    if(isUpdate && pCategoryId!=='0') {
+      // 获取对应的二级分类列表
+      const subCategorys = await this.getCategorys(pCategoryId)
+      // 生成二级下拉列表的options
+      const childOptions = subCategorys.map(c => ({
+        value: c._id,
+        label: c.name,
+        isLeaf: true
+      }))
+
+      // 找到当前商品对应的一级option对象
+      const targetOption = options.find(option => option.value===pCategoryId) */
+
+      // 关联对应的一级option上
+      //targetOption.children = childOptions
+    }
+
+
+   /*  // 更新options状态
+    this.setState({
+      options
+    }) */
+  //}
+
+
+  /*
+  异步获取一级/二级分类列表, 并显示
+  async函数的返回值是一个新的promise对象, promise的结果和值由async的结果来决定
+   */
+  getCategorys = async (parentId) => {
+    const result = await reqCategorys(parentId)   // {status: 0, data: categorys}
+    if (result.status===0) {
+      const categorys = result.data
+      // 如果是一级分类列表
+      if (parentId==='0') {
+        this.initOptions(categorys)
+      } else { // 二级列表
+        return categorys  // 返回二级列表 ==> 当前async函数返回的promsie就会成功且value为categorys
+      }
+    }
+  }
+
+
+   /*
+  验证价格的自定义验证函数
+   */
+  validatePrice = (rule, value, callback) => {
+    console.log(value, typeof value)
+    if (value*1 > 0) {
+      callback() // 验证通过
+    } else {
+      callback('价格必须大于0') // 验证没通过
+    }
+  }
+
+  /*
+  用加载下一级列表的回调函数
+   */
+  loadData = async selectedOptions => {
+    // 得到选择的option对象
+    const targetOption = selectedOptions[0]
+    // 显示loading
+    targetOption.loading = true
+
+    // 根据选中的分类, 请求获取二级分类列表
+    const subCategorys = await this.getCategorys(targetOption.value)
+    // 隐藏loading
+    targetOption.loading = false
+    // 二级分类数组有数据
+    /* if (subCategorys && subCategorys.length>0) { */
+      // 生成一个二级列表的options
+      const childOptions = subCategorys.map(c => ({
+        value: c._id,
+        label: c.name,
+        isLeaf: true
+      }))
+      // 关联到当前option上
+      targetOption.children = childOptions
+   /*  } else { // 当前选中的分类没有二级分类
+      targetOption.isLeaf = true
+    } */
+
+    // 更新options状态
+    this.setState({
+      options: [...this.state.options],
+    })
+  }
+
+
+
+ // 创建ref
+ addUpdateForm = React.createRef()
+ pw = React.createRef() //接收图片的ref
+ editor = React.createRef() //富文本的ref
+ // 提交更新或者修改
+ submit = async()=>{
+   let values
+   try{
+     values = await this.addUpdateForm.current.validateFields()
+     // 取出数据
+   const {name,desc,price,categoryIds} = values
+   
+   let pCategoryId, categoryId
+       if (categoryIds.length===1) {
+         pCategoryId = '0'
+         categoryId = categoryIds[0]
+       } else {
+         pCategoryId = categoryIds[0]
+         categoryId = categoryIds[1]
+       }
+   const imgs = this.pw.current.getImgs()
+   const detail = this.editor.current.getDetail()
+   const product = {name, desc, price, imgs, detail, pCategoryId, categoryId}
+   // 如果是更新, 需要添加_id
+   if(this.isUpdate){
+     product._id = this.products._id
+   }
+   // 2. 调用接口请求函数去添加/更新
+   const result = (await reqAddOrUpdateProduct(product)).data
+   // 3. 根据结果提示
+   if (result.status===0) {
+     message.success(`${this.isUpdate ? '更新' : '添加'}商品成功!`)
+     this.props.navigate(-1)
+   } else {
+     message.error(`${this.isUpdate ? '更新' : '添加'}商品失败!`)
+   }
+   }
+   catch({errorFields,values}){
+     console.log(errorFields)
+     if(errorFields.length>0){
+       errorFields.map(err=>{
+        return message.error(err.errors)
+       })
+     }
+   }
+   
+   }
+   // console.log(this.addUpdateForm.current)
+   // console.log(this.addUpdateForm.current.getFieldsValue())
+   // console.log(this.pw.current.getImgs(),this.editor.current.getDetail())
+
+
+
+   componentDidMount () {
+    this.getCategorys('0')
+  }
+
+
+
+
+
   render() {
      // 指定Item布局的配置对象
      const formItemLayout = {
@@ -76,9 +246,8 @@ export default class ProductAddUpdate extends Component {
           name='price'
          /*  initialValue={price} */
           rules={[
-            {
-              required: true, message: '必须输入商品价格'
-            }
+            {required: true, message: '必须输入商品价格'},
+            {validator: this.validatePrice}
           ]}
           >
             <InputNumber addonAfter="元"/>
@@ -89,8 +258,8 @@ export default class ProductAddUpdate extends Component {
           /* initialValue={categoryIds} */
           >
             <Cascader 
-            //options={this.state.options} 
-           // loadData={this.loadData}
+            options={this.state.options} 
+            loadData={this.loadData}
             placeholder='请指定商品分类'
             rules={[
               {
